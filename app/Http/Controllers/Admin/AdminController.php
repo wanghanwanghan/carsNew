1335dc0a3670212252f562dcc0987d1ebafec71a
wanghan <minglongoc@me.com>
2020-06-30 15:07:28 +0800

    321

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -10,6 +10,8 @@ class AdminController extends AdminBase
     //后台用户登录
     public function login(Request $request)
     {
+
+        dd($request->all());
         $username=$request->username;
         $password=$request->password;
 
