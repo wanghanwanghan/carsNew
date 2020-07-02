2be90c94ba975d9d6e118770085f093adbe177d4
wanghan <minglongoc@me.com>
2020-07-02 18:09:35 +0800

    321

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        HandleCors::class,
     ];
 
     /**
@@ -38,11 +37,13 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            HandleCors::class,
         ],
 
         'api' => [
             'throttle:120,1',
             'bindings',
+            HandleCors::class,
         ],
     ];
 
