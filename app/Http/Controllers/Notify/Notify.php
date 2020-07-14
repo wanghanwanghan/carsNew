2d82ef1002dd0b72a7d67fa25cde45a266e0d805
wanghan <minglongoc@me.com>
2020-07-14 10:46:39 +0800

    321

--- app/Http/Controllers/Notify/Notify.php
+++ app/Http/Controllers/Notify/Notify.php
@@ -3,14 +3,33 @@
 namespace App\Http\Controllers\Notify;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
+use Yansongda\LaravelPay\Facades\Pay;
 
 class Notify extends Controller
 {
     public function wxNotify(Request $request)
     {
-        Redis::set('minipay',json_encode($request->all()));
+        $pay=Pay::wechat();
+
+        $key='MiniPay_'.Carbon::now()->format('YmdHis');
+
+        try
+        {
+            $data=$pay->verify();
+
+            Redis::set($key,json_encode($data));
+
+        }catch (\Exception $e)
+        {
+            Redis::set($key,json_encode($e->getMessage()));
+        }
+
+        Redis::expire($key,600);
+
+        return $pay->success()->send();
     }
 
 }
