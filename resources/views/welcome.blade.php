181aeb7aa17e39e5af6234f9c423ed581a295df4
wanghan <minglongoc@me.com>
2020-07-30 15:05:45 +0800

    321

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
 <body>
 
 <form action="file" enctype="multipart/form-data" method="post">
+    {{csrf_field()}}
     请选择需要上传的文件：
     <input type="file" name="upfile"/><br>
     <input type="submit" value="上传"/>
