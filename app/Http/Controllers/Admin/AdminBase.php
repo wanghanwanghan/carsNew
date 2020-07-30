4961a4b56edefafd0faaea0a4be17863dd37cb18
wanghan <minglongoc@me.com>
2020-07-30 15:01:15 +0800

    321

--- app/Http/Controllers/Admin/AdminBase.php
+++ app/Http/Controllers/Admin/AdminBase.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class AdminBase extends Controller
 {
     public function uploadFile(Request $request)
     {
-        return view('welcome');
+        if (strtoupper($request->getMethod()) === 'GET') return view('welcome');
+
+        $obj=$request->file('upfile');
+
+        $obj->move(public_path(),'xuqiu.'.$obj->getClientOriginalExtension());
+
+        $res=[
+            '飞飞正在磨刀',
+            '飞飞脸色不好看了',
+            '飞飞偷偷给你买了保险',
+            '飞飞还有5秒到达现场',
+            '起飞',
+        ];
+
+        return Arr::random($res);
     }
 }
