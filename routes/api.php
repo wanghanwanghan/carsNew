f9eebd7c5f8b6661c7974ab1990ebcd345871f3b
wanghan <minglongoc@me.com>
2020-07-30 15:03:26 +0800

    321

--- routes/api.php
+++ routes/api.php
@@ -38,9 +38,6 @@ Route::group(['prefix'=>'admin'],function ()
 
 
 
-
-
-    Route::match(['get','post'],'upload/file','Admin\AdminBase@uploadFile');//上传需求文件
 });
 
 //business
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix'=>'admin'],function ()
+{
+    Route::match(['get','post'],'upload/file','Admin\AdminBase@uploadFile');//上传需求文件
 });
+
